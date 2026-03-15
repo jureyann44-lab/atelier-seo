e1e7507954428bb8dcd3c07658058ac91ee2f08e
Yann Jure <jure.yann44@gmail.com>
2026-03-15 11:21:03 +0100

    fix: unsafe-eval dans CSP pour Decap CMS admin

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,14 @@ import { defineMiddleware } from 'astro:middleware';
 
 const CSP_DEFAULT =
   "default-src 'self'; " +
-  "connect-src 'self' https://proud-bush-8032atelier-seo-contact-form.jure-yann44.workers.dev https://cloudflareinsights.com; " +
-  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cloudflareinsights.com https://static.cloudflareinsights.com; " +
-  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
-  "img-src 'self' data: https:; " +
-  "font-src 'self' data: https://fonts.gstatic.com;";
+  "connect-src 'self' https://proud-bush-8032atelier-seo-contact-form.jure-yann44.workers.dev https://cloudflareinsights.com https://api.github.com https://raw.githubusercontent.com https://github.com https://unpkg.com; " +
+  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cloudflareinsights.com https://static.cloudflareinsights.com https://unpkg.com; " +
+  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com; " +
+  "img-src 'self' data: blob: https:; " +
+  "font-src 'self' data: https://fonts.gstatic.com; " +
+  "frame-src https://github.com; " +
+  "form-action 'self' https://github.com; " +
+  "worker-src 'self' blob:;";
 
 export const onRequest = defineMiddleware(async ({ url }, next) => {
   const response = await next();
