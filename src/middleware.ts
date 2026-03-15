261b223f7752329df9ed4bba00ce871a92c07fa4
Yann Jure <jure.yann44@gmail.com>
2026-03-15 05:11:02 +0100

    Fix CSP middleware for Decap CMS

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { defineMiddleware } from 'astro:middleware';
 const CSP_DEFAULT =
   "default-src 'self'; " +
   "connect-src 'self' https://proud-bush-8032atelier-seo-contact-form.jure-yann44.workers.dev https://cloudflareinsights.com; " +
-  "script-src 'self' 'unsafe-inline' https://cloudflareinsights.com https://static.cloudflareinsights.com; " +
+  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cloudflareinsights.com https://static.cloudflareinsights.com; " +
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
   "img-src 'self' data: https:; " +
   "font-src 'self' data: https://fonts.gstatic.com;";
